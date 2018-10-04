a715882c0df826431d6f6ff1cc47afefa7e09e7f
fobos fobos <865peadd@gmail.com>
2018-10-04 16:59:19 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp12/Form1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+	public partial class Form1 : Form
+	{
+		product p = new product { Name="test"};
+		public Form1()
+		{
+			InitializeComponent();
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				foreach (product u in db.Users.ToList())
+				{
+					listBox1.Items.Add($"{u.Id}.{u.Name} - {u.Price}");
+				}
+			}
+		}
+
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+			Form2 myForm = new Form2(this, p);
+
+			if (myForm.ShowDialog(this) == DialogResult.OK)
+			{
+				listBox1.Items.Clear();
+				p = myForm.MyProperty;
+				MessageBox.Show(p.Name);
+				using (ApplicationContext db = new ApplicationContext())
+				{
+					db.Users.Add(p);
+
+					db.SaveChanges();
+
+					var products = db.Users.ToList();
+					foreach (product u in products)
+					{
+						listBox1.Items.Add($"{u.Id}.{u.Name} - {u.Price}");
+					}
+				}
+			}
+			myForm.Dispose();
+		}
+
+		private void button2_Click_1(object sender, EventArgs e)
+		{
+			using (ApplicationContext db = new ApplicationContext())
+			{
+				db.Database.EnsureDeleted();
+				db.SaveChanges();
+				listBox1.Items.Clear();
+			}
+		}
+	}
+}
---
+++ WindowsFormsApp12/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+	public partial class Form2 : Form
+	{
+		public Form2()
+		{
+			InitializeComponent();
+		}
+		product pr;
+		public product MyProperty
+		{
+			get
+			{
+				return pr;
+			}
+		}
+		public Form2(Form1 f, product p)
+		{
+			pr = p;
+			InitializeComponent();
+			textBox1.Text = p.Name;
+			button1.DialogResult = DialogResult.OK;
+		}
+
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+			pr = new product { Name = textBox1.Text, Price = Convert.ToInt32(textBox2.Text) };
+
+		}
+	}
+}
